652b6a449518d72779a199be753e12f091c40608
Huanyue <1582884964@qq.com>
2020-02-22 18:26:08 +0800

    两数之和

--- 两数之和.js
+++ 两数之和.js
@@ -20,3 +20,23 @@ var twoSum = function(nums, target) {
   };
 // @lc code=end
 
+
+// 1.使用一层循环，每遍历到一个元素就计算该元素与 targettarget 之间的差值 difdif，然后以 difdif 为下标到数组temp中寻找，如果 temp[dif] 有值(即不是 undefinedundefined)，则返回两个元素在数组 numsnums 的下标，如果没有找到，则将当前元素存入数组 temptemp 中(下标: nums[i], Value: inums[i],Value:i) 。
+// 2.时间复杂度：O(n)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  var temp = [];
+  for(var i=0;i<nums.length;i++){
+      var dif = target - nums[i];
+      if(temp[dif] != undefined){
+          return [temp[dif],i];
+      }
+      temp[nums[i]] = i;
+  }
+};
+
+
