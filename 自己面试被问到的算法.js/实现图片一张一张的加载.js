8c7806e818d93300126bbc18b1c6a6c06e42b6b2
Huanyue <1582884964@qq.com>
2020-03-22 11:51:58 +0800

    阿里面试算法题

---
+++ 自己面试被问到的算法.js/实现图片一张一张的加载.js
@@ -0,0 +1,31 @@
+// 有一个数组：
+// const imgs = ['url1','url2','url3',...];
+// 请实现效果：
+// 请按照图片数组顺序队列加载图片（注：加载完一张再加载下一张）
+
+const images = ['url1','url2','url3']
+
+const loadImg = (url) => {
+ return new Promise(resolve => {
+   let image = new Image()
+   image.src = url
+   image.onload = () =>{
+     resolve(image)
+   }
+   image.onerror = () => {
+     reject(`Errors on loading`)
+   }
+ }) 
+}
+
+const solve = (images) => {
+    const imgQueue = []
+    for(let i = 0;i<images.length;i++) {
+      imgQueue.push(loadImg(images[i]))
+    }
+    Promise.all(imgQueue).then((item) => {
+      console.log(item)
+    })
+}
+
+solve(images)
