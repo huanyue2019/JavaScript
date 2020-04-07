aa3ec9e8a4c1a09388feeb5e7de06c2c37da1b8c
huanyue2019 <1582884964@qq.com>
2020-04-08 06:53:37 +0800

    两数之和再看

--- leetcode/两数之和.js
+++ leetcode/两数之和.js
@@ -13,15 +13,18 @@
 var twoSum = function(nums, target) {
     const map = new Map()
     for (let i = 0; i < nums.length; i ++) {
-      const otherIndex = map.get(target - nums[i])
+      const otherIndex = map.get(target - nums[i]) // 把target当成map的一个键，使用get方法读取这个值。如果找不到键值，返回undefined。
       if (otherIndex !== undefined) return [otherIndex, i]
-      map.set(nums[i], i)
+      map.set(nums[i], i); // 返回当前map对象。
     }
   };
-// @lc code=end
+// map.get()   get() 方法返回某个 Map 对象中的一个指定元素。
+// map.set()   set() 方法为 Map 对象添加或更新一个指定了键（key）和值（value）的（新）键值对。
 
 
-// 1.使用一层循环，每遍历到一个元素就计算该元素与 targettarget 之间的差值 difdif，然后以 difdif 为下标到数组temp中寻找，如果 temp[dif] 有值(即不是 undefinedundefined)，则返回两个元素在数组 numsnums 的下标，如果没有找到，则将当前元素存入数组 temptemp 中(下标: nums[i], Value: inums[i],Value:i) 。
+// 1.使用一层循环，每遍历到一个元素就计算该元素与 targettarget 之间的差值 dif，然后以 dif 为下标到数组temp中寻找，
+// 如果 temp[dif] 有值(即不是 undefinedundefined)，则返回两个元素在数组 numsnums 的下标，如果没有找到，
+// 则将当前元素存入数组 temptemp 中(下标: nums[i], Value: inums[i],Value:i) 。
 // 2.时间复杂度：O(n)
 /**
  * @param {number[]} nums
